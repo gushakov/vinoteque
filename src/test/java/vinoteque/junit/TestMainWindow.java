c56b7809d8b9e9f17bddbb10c583bd590bdaff22
George Ushakov <gushakov@yahoo.com>
2012-04-19 12:22:20 +0200

    removing personal data

--- src/test/java/vinoteque/junit/TestMainWindow.java
+++ src/test/java/vinoteque/junit/TestMainWindow.java
@@ -1,14 +1,13 @@
 package vinoteque.junit;
 
 import java.util.Properties;
-import viniteque.config.AppConfig;
 import org.apache.log4j.Logger;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import vinoteque.utils.Utils;
+import viniteque.config.AppConfig;
 
-import static org.junit.Assert.*;
 
 public class TestMainWindow {
     private static final Logger logger = Logger.getLogger(TestMainWindow.class);
@@ -16,7 +15,9 @@ public class TestMainWindow {
     @Test
     public void getAppVersion() throws Exception {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-        Utils.writeProperties((Properties)ctx.getBean("appProps"));
+        //Utils.writeProperties((Properties)ctx.getBean("appProps"));
+        Properties props = (Properties) ctx.getBean("appProps");
+        assertNotNull(props);
     }
 
 }

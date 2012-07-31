cc30b236921ceecfc6ba2adb35498fb06c17d7e4
George Ushakov <gushakov@yahoo.com>
2012-07-31 18:54:39 +0200

    updated create vins script

--- hsqldb/vins_create.sql
+++ hsqldb/vins_create.sql
@@ -9,5 +9,7 @@ create table public.vins (
     vigneron varchar(100),
     qualite varchar(5),
     stock int,
-    prix_btl decimal(10,2)
+    prix_btl decimal(100),
+    annee_consommation integer,
+    commentaire varchar(1000)
 );
